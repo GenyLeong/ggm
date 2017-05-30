ff10ae68e57812e66b7c35400c3585c8f56c811d
genesis.leon <genesis.leon@glr.pe>
2017-05-30 16:38:50 -0500

    avance

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,39 @@ function resize_fondo(){
 }
 
 (function() {
+  var $ = Sizzle,
+      $panel1 = $("#panel1"),
+      $panel2 = $("#panel2"),
+      $panel3 = $("#panel3"),
+      $panel1Text = $("#panel1 h1"),
+      $panel2Text = $("#panel2 h2"),
+      $info = $("#info"),
+      $list = $("li"),
+      $orderNow = $("#orderNow"),
+      tl;
+
+  var tl = new TimelineMax({delay:0.5, repeat:0, repeatDelay:0});
+
+  //banner animation code (only 11 lines)
+
+   tl.from(panel1, 0.5, {autoAlpha:0})
+     .from($panel1Text, 0.5, {scale:0.5, autoAlpha:0, ease:Back.easeOut})
+     .set($panel2, {top:0}, "+=2")
+     .from($panel2, 0.2, {autoAlpha:0, scale:1.5})
+     .from($panel2Text, 0.2, {top:250}, "+=0.5")
+     .to($panel2Text, 0.2, {top:0}, "+=1")
+     .set($panel3, {top:0}, "final")
+     .from($info, 0.2, {autoAlpha:0, scale:1.5})
+     .to($panel2, 0.5, {top:0}, "final")
+     .staggerFrom($list, 0.3, {autoAlpha:0, left:50}, 0.1, "+=0.2")
+     .from($orderNow, 0.5, {scale:0, autoAlpha:0, ease:Back.easeOut});
+
+// function restart(){
+//   var info = document.getElementById("info")
+//   info.style.height = "100%";
+// }
+
+/*texto*/
     var controller = new ScrollMagic.Controller();
     var var_1, var_2, var_3, var_4,
         scene_1, scene_2, scene_3, scene_4, scene_5, scene_6, scene_7, scene_8, scene_9,
